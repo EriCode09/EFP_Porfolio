99fd85f6e87b2ca90bd17f6fb8ee124c2b82fff3
efernandez09 <111697242+efernandez09@users.noreply.github.com>
2023-10-08 01:37:22 +0200

    Proyecto Creado

---
+++ EFP_Portfolio/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import MenuNavbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+
+      <MenuNavbar/>
+
+      <p className="text-3xl font-bold underline">Hello World</p>
+
+    </>
+  )
+}
+
+export default App
---
+++ EFP_Portfolio/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const HomePage = () => {
+
+
+
+    return(
+        <div>
+        
+        <p>Home Page</p>
+        
+        </div>
+    )
+
+}
+
+export default HomePage
